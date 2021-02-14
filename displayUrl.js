aabd568be3d3b8c6d6ac8391a4ad423f585a3af1
Yuva <ymahendran@integralads.com>
2021-02-14 09:26:02 -0500

    adding first library to display current url at the end of body tag

---
+++ displayUrl.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName('body').innerHTML += "<br/><br/><div id='debugUrl' style='font-size: 7px;'></div>";
+document.getElementById('debugUrl').innerHTML = window.location.href;
